af90a89b3ef01592201b9682a1031a34583d0349
paulcoding810 <41385034+longnghia@users.noreply.github.com>
2025-02-16 09:16:11 +0700

    add generate script

---
+++ generate.js
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+
+import fs from "fs-extra";
+import ignore from "ignore";
+import inquirer from "inquirer";
+import { fileURLToPath } from "node:url";
+import path from "path";
+
+async function generateProject() {
+  const answers = await inquirer.prompt([
+    { name: "name", message: "Project name:", default: "my-crx-app" },
+    { name: "version", message: "Version:", default: "0.0.0" },
+    { name: "description", message: "Description:", default: "" },
+    { name: "author", message: "Author:", default: "" },
+  ]);
+
+  const templateDir = path.resolve(
+    fileURLToPath(import.meta.url),
+    "..",
+    "template"
+  );
+
+  const outputDir = path.join(process.cwd(), answers.name);
+
+  // await fs.copy(templateDir, outputDir);
+  // console.log(`Copied template to ${outputDir}`);
+
+  // Load .gitignore rules
+  const gitignorePath = path.join(templateDir, ".gitignore");
+  let ig = ignore();
+  if (fs.existsSync(gitignorePath)) {
+    const gitignoreContent = fs.readFileSync(gitignorePath, "utf8");
+    ig = ignore().add(gitignoreContent);
+  }
+
+  // Copy template with .gitignore filtering
+  await fs.copy(templateDir, outputDir, {
+    filter: (src) => {
+      const relativePath = path.relative(templateDir, src);
+      return relativePath === "" || !ig.ignores(relativePath);
+    },
+  });
+
+  // Update package.json
+  const packageJsonPath = path.join(outputDir, "package.json");
+  let packageJson = await fs.readFile(packageJsonPath, "utf-8");
+
+  const pkg = JSON.parse(packageJson, "utf-8");
+  pkg.name = answers.name;
+  pkg.displayName = answers.name;
+  pkg.version = answers.version;
+  pkg.description = answers.description;
+  pkg.author = answers.author;
+
+  await fs.writeFile(packageJsonPath, JSON.stringify(pkg, null, 2));
+
+  // Update README.md
+  const readmePath = path.join(outputDir, "README.md");
+  let readmeContent = await fs.readFile(readmePath, "utf8");
+  readmeContent = readmeContent.replace(/my-crx-app/g, answers.name);
+  await fs.writeFile(readmePath, readmeContent);
+
+  console.log(`
+   Suggest you next step:
+    1. cd ${path.relative(process.cwd(), outputDir)}
+    2. Run yarn install
+    3. Open chrome://extensions/ in your browser
+    4. Check the box for Developer mode in the top right.
+    5. Click the Load unpacked extension button.
+    6. Select the build/ directory that was created.
+  `);
+}
+
+generateProject().catch(console.error);
