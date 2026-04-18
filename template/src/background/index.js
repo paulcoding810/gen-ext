d259ccea364bf7b6bbd6c30ffb3b87c20abef63c
paulcoding810 <longnghia2.00@gmail.com>
2026-04-18 20:04:32 +0700

    feat: add onInstalled listener to open options page on first install

--- template/src/background/index.js
+++ template/src/background/index.js
@@ -1,5 +1,12 @@
 console.log('background is running')
 
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason !== 'install') {
+    return
+  }
+  chrome.runtime.openOptionsPage()
+})
+
 chrome.runtime.onMessage.addListener((request) => {
   if (request.type === 'COUNT') {
     console.log('background has received a message from popup, and count is ', request?.count)
